476a4045555b06d2b682c96e7460c24458d3bc42
Hiroyuki <Hiroyuki@Mae-PC>
2013-10-29 22:39:58 +0900

    Add OptionTest.java

---
+++ src/test/java/mockexamtool/examination/OptionTest.java
@@ -0,0 +1,78 @@
+/**
+ * 
+ */
+package mockexamtool.examination;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author Hiroyuki
+ *
+ */
+public class OptionTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * {@link mockexamtool.examination.Option#Option(java.lang.String, boolean)} のためのテスト・メソッド。
+	 */
+	@Test
+	public void testOption() {
+		Option option = new Option("", false);
+		fail("まだ実装されていません");
+	}
+
+	/**
+	 * {@link mockexamtool.examination.Option#getSentence()} のためのテスト・メソッド。
+	 */
+	@Test
+	public void GetSentence_正常() {
+		String expected = "sentence";
+		Option option = new Option(expected, false);
+		String actual = option.getSentence();
+		
+		assertThat(actual, is(expected));
+	}
+
+	/**
+	 * {@link mockexamtool.examination.Option#isAnswer()} のためのテスト・メソッド。
+	 */
+	@Test
+	public void IsAnswer_真の場合() {
+		boolean expected = true;
+		Option option = new Option("sentence", expected);
+		boolean actual = option.isAnswer();
+		
+		assertThat(actual, is(expected));
+	}
+
+	/**
+	 * {@link mockexamtool.examination.Option#isAnswer()} のためのテスト・メソッド。
+	 */
+	@Test
+	public void IsAnswer_偽の場合() {
+		boolean expected = false;
+		Option option = new Option("sentence", expected);
+		boolean actual = option.isAnswer();
+		
+		assertThat(actual, is(expected));
+	}
+
+}
